7bd8e1f377b752343f6b6425affb4ce8d93054f5
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-05-18 12:54:36 +0100

    Implement authentication flow with Clerk and update package scripts

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { useAuth } from '@clerk/expo';
+import { Redirect, Stack } from 'expo-router';
+
+export default function AuthRoutesLayout() {
+  const { isSignedIn, isLoaded } = useAuth()
+
+  if (!isLoaded) {
+    return;
+  }
+
+  if (isSignedIn) {
+    return <Redirect href={'/'} />
+  }
+
+  return <Stack />
+}
